819f48c47a13f0e5d58ba107ac83b1ef7f7748e8
Davi <davivitoriosom.com@gmail.com>
2023-08-31 21:26:26 -0300

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+  import { onMount } from "svelte";
+  import Pusher from "pusher-js";
+
+  let username = "username";
+  let message = "";
+  let messages = [];
+
+  onMount(() => {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher("4b138ef50a1b4c6f3d43", {
+      cluster: "us2",
+    });
+
+    const channel = pusher.subscribe("chat");
+    channel.bind("message", (data) => {
+      messages = [...messages, data];
+    });
+  });
+
+  const submit = async () => {
+    await fetch("http://localhost:8000/api/messages", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        message,
+      }),
+    });
+
+    message = "";
+  };
+</script>
+
+<div class="container">
+  <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+    <div
+      class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
+    >
+      <input class="fs-5 fw-semibold" bind:value={username} />
+    </div>
+    <div class="list-group list-group-flush border-bottom scrollarea">
+      {#each messages as msg}
+        <div class="list-group-item list-group-item-action py-3 lh-tight">
+          <div class="d-flex w-100 align-items-center justify-content-between">
+            <strong class="mb-1">{msg.username}</strong>
+          </div>
+          <div class="col-10 mb1 small">{msg.message}</div>
+        </div>
+      {/each}
+    </div>
+  </div>
+  <form on:submit|preventDefault={submit}>
+    <input
+      class="form-control"
+      placeholder="Write a message"
+      bind:value={message}
+    />
+  </form>
+</div>
+
+<style>
+  .scrollarea {
+    min-height: 500px;
+  }
+</style>
